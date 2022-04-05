98fc762524058a0213214388e95885336f99a9d8
gildiamant31 <gil.diamant@live.biu.ac.il>
2022-04-05 22:49:45 +0300

    add dummy script & .gitignore

---
+++ ex1/main.py
@@ -0,0 +1,12 @@
+import pygame
+import sys
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+matrix_size = 200 , 200
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(matrix_size)
